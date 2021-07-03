f22f10cca318dba05dba922dd2b31223703f2aee
Thiago Costa <thiagohrcosta86@gmail.com>
2021-07-03 15:57:05 -0300

    Course List Manager Bootcamp Santander

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from "@angular/router";
+import { HttpClientModule } from "@angular/common/http";
+
+import { AppComponent } from './app.component';
+import { StarComponent } from './star/star.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { Error404Component } from './error-404/error-404.component';
+import { CourseModule } from './courses/course.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    Error404Component,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    CourseModule,
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
+      },
+      {
+        path: '**', component: Error404Component
+      }
+    ])
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/courses/course.ts
@@ -0,0 +1,11 @@
+export class Course {
+  id: number | undefined;
+  name: string = "";
+  imageUrl: string | undefined;
+  price: number | undefined;
+  code: string = "";
+  duration: number | undefined;
+  rating: number = 0;
+  releaseDate: string | undefined;
+  description: string | undefined;
+}
